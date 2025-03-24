72fa460d4abf684e3fa5ff12255d609c106f65c9
Alok Rai <101451098+AlokRai123@users.noreply.github.com>
2025-03-24 23:26:48 +0530

    mostly completed

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,8 +1,107 @@
-import React from 'react'
+'use client'
+import { zodResolver } from "@hookform/resolvers/zod"
 
-function page() {
+import { useForm } from "react-hook-form"
+import * as z  from "zod"
+import Link from "next/link"
+import { toast } from "sonner"
+import { useRouter } from "next/navigation"
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { signInSchema } from "@/schemas/signInSchema"
+import { signIn } from "next-auth/react"
+
+
+const page = () => {
+
+  const router = useRouter()
+
+  //zod implementation
+  const form = useForm<z.infer<typeof signInSchema>>({
+    resolver : zodResolver(signInSchema),
+    defaultValues : {
+
+      email : '',
+      password : ''
+    }
+  })
+
+
+
+  const onSubmit = async (data : z.infer<typeof signInSchema>) => {
+
+  const result = await signIn('credentials',{
+    redirect : false,
+    identifier : data.identifier,
+    password : data.password
+  })
+
+  if(result?.error){
+     toast({
+      title : "login Failed",
+      description : "Incorrect username or password",
+      variant : "destructive"
+     })
+  }
+
+  if(result?.url)
+    router.replace('/dashboard')
+  }
+
   return (
-    <div>sign-in page</div>
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shodow-md">
+        <div className="text-center">
+           <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">Join Mystery Message</h1>
+           <p className="mb-4">Sign In to start your anonymous adventure</p>
+        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+
+             <FormField
+              control={form.control}
+              name="identifier"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username/email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Email/Username" {...field}  />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                     type="password"
+                    placeholder="Password" {...field}  />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">
+              Signin
+            </Button>
+          </form>
+        </Form>
+        <div className="text-center mt-4">
+          <p>
+            Already a member?{''}
+            <Link href = "/sign-in" className="text-blue-600 hover:text-blue-800">
+              Sign in
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -10,7 +10,7 @@ import { useRouter } from "next/navigation"
 import { signUpSchema } from "@/schemas/signUpSchema"
 import axios,{AxiosError} from 'axios'
 import { ApiResponse } from "@/types/ApiResponse"
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 
@@ -64,7 +64,7 @@ const page = () => {
       const response = await axios.post<ApiResponse>('/api/sign-up',data)
 
       toast({
-        title : 'Success',
+        tittle : 'Success',
         description : response.data.message
       })
       router.replace(`/verify${username}`)
